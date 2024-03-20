2c68039b673330f8cc3afff591704a37fb6bd6da
HOUESSOU Béryl <beryl.houessou@gmail.com>
2024-03-20 14:39:14 +0000

    End of  1-country_users.sql

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- 1. In and not out
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        name VARCHAR(255),
+        country enum ('US', 'CO', 'TN') default 'US'
+    );
